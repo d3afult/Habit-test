5507a27884999928445da94eef8be65b68700788
Slyman <gunnersly@protonmail.com>
2026-02-09 13:20:12 +0000

    add users table db.py

--- db.py
+++ db.py
@@ -1,4 +1,3 @@
-# db.py
 import sqlite3
 from contextlib import contextmanager
 
@@ -9,6 +8,7 @@ def get_conn():
     conn = sqlite3.connect(DB_PATH, check_same_thread=False)
     conn.row_factory = sqlite3.Row
     try:
+        conn.execute("PRAGMA foreign_keys = ON;")
         yield conn
     finally:
         conn.commit()
@@ -17,20 +17,35 @@ def get_conn():
 def init_db():
     with get_conn() as conn:
         conn.execute("""
-        CREATE TABLE IF NOT EXISTS habits (
+        CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            active INTEGER NOT NULL DEFAULT 1,
+            username TEXT NOT NULL UNIQUE,
+            pw_hash TEXT NOT NULL,
+            pw_salt TEXT NOT NULL,
             created_at TEXT NOT NULL
         )
         """)
+
+        conn.execute("""
+        CREATE TABLE IF NOT EXISTS habits (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            name TEXT NOT NULL,
+            active INTEGER NOT NULL DEFAULT 1,
+            created_at TEXT NOT NULL,
+            FOREIGN KEY(user_id) REFERENCES users(id)
+        )
+        """)
+
         conn.execute("""
         CREATE TABLE IF NOT EXISTS completions (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
             habit_id INTEGER NOT NULL,
             day TEXT NOT NULL,              -- YYYY-MM-DD
             completed_at TEXT NOT NULL,
-            UNIQUE(habit_id, day),
+            UNIQUE(user_id, habit_id, day),
+            FOREIGN KEY(user_id) REFERENCES users(id),
             FOREIGN KEY(habit_id) REFERENCES habits(id)
         )
         """)
